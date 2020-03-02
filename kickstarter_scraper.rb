f4cdb36838ce28ced8b8b32fc00e5b6c1d6a59af
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-02 21:24:46 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,6 +10,10 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   projects = {}
   
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+  
 end
 
 create_project_hash
